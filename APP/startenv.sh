9450d4ef8c185599e6c8df8a0e99d7a4e31fd995
buhtig-sudo-azar <167521712+buhtig-sudo-azar@users.noreply.github.com>
2025-11-12 09:25:36 +0000

    first commit

---
+++ APP/startenv.sh
@@ -0,0 +1,18 @@
+source env/bin/activate
+
+
+
+#   GNU nano 7.2                                                        /home/codespace/.bashrc                                                                  
+# cd() {
+   builtin cd "$@" || return
+
+# # Активировать окружение SEQURITY при входе
+#   if [[ "$PWD" == *"/simple_ai_tutor"* ]]; then
+#    source startenv.sh
+#   fi
+  # Деактивировать окружение, если выход из SEQURITY
+#   if [[ "$OLDPWD" == *"/simple_ai_tutor"* ]] && [[ "$PWD" != *"/simple_ai_tutor"* ]]; then
+#     deactivate 2>/dev/null
+#   fi
+#   }
+#
