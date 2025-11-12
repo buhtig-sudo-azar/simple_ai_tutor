414235d931e184fe9e7a7f9432001362f945a53c
buhtig-sudo-azar <167521712+buhtig-sudo-azar@users.noreply.github.com>
2025-11-12 12:06:09 +0000

    1

--- APP/startenv.sh
+++ APP/startenv.sh
@@ -2,17 +2,17 @@ source env/bin/activate
 
 
 
-#   GNU nano 7.2                                                        /home/codespace/.bashrc                                                                  
-# cd() {
-   builtin cd "$@" || return
+ #  GNU nano 7.2                                                        /home/codespace/.bashrc                                                                  
+ # cd() {
+ #   builtin cd "$@" || return
 
-# # Активировать окружение SEQURITY при входе
-#   if [[ "$PWD" == *"/simple_ai_tutor"* ]]; then
-#    source startenv.sh
-#   fi
-  # Деактивировать окружение, если выход из SEQURITY
-#   if [[ "$OLDPWD" == *"/simple_ai_tutor"* ]] && [[ "$PWD" != *"/simple_ai_tutor"* ]]; then
-#     deactivate 2>/dev/null
-#   fi
-#   }
-#
+ #  Активировать окружение SEQURITY при входе
+ #   if [[ "$PWD" == *"/APP"* ]]; then
+ #    source startenv.sh
+ #   fi
+ # Деактивировать окружение, если выход из SEQURITY
+ #   if [[ "$OLDPWD" == *"/APP"* ]] && [[ "$PWD" != *"/APP"* ]]; then
+ #     deactivate 2>/dev/null
+ # fi
+ #  }
+
