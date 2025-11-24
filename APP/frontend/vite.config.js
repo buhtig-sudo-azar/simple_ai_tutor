edccf5520d4bc9f6abcc1edacb3558d5f682de6a
buhtig-sudo-azar <167521712+buhtig-sudo-azar@users.noreply.github.com>
2025-11-24 05:55:03 +0000

    1

--- APP/frontend/vite.config.js
+++ APP/frontend/vite.config.js
@@ -3,18 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',  // ← ДОБАВЬ ЭТУ СТРОЧКУ!
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://bug-free-palm-tree-69vq4xw7q7j9256xg-5000.app.github.dev',
-        changeOrigin: true,
-        secure: false
-      }
-    }
-  },
+  base: '/',  // Убери './' попробуй просто '/'
   build: {
     outDir: 'dist',
-    emptyOutDir: true
+    emptyOutDir: true,
+    assetsDir: 'assets'
   }
 })
